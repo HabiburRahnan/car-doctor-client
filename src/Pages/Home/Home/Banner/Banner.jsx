665557e25d42354209cd3f83b66bf382664b5fa4
habibur rahman <habiburrahmanofficial157@gmail.com>
2023-10-23 17:07:06 +0600

    create a banner section a slider and responsive code

--- src/Pages/Home/Home/Banner/Banner.jsx
+++ src/Pages/Home/Home/Banner/Banner.jsx
@@ -5,22 +5,56 @@ import img4 from "../../../../assets/images/homeCarousel/4.jpg";
 
 const Banner = () => {
   return (
-    <div className="carousel w-full">
+    <div className="carousel w-full h-[350px] md:h-[600px]">
       <div id="slide1" className="carousel-item relative w-full">
-        <img src={img1} className="w-full" />
-        <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0 gap-5">
-          <a href="#slide4" className="btn btn-circle">
+        <img src={img1} className="w-full rounded-xl" />
+        <div className="absolute h-full flex items-center  left-0  top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+          <div className="text-white space-y-3 md:space-y-7 pl-12 w-1/2">
+            <h2 className="text-2xl md:text-6xl font-bold">
+              Affordable Price For Car Servicing
+            </h2>
+            <p>
+              There are many variations of passages of available, but the
+              majority have suffered alteration in some form
+            </p>
+            <div>
+              <button className="btn btn-primary mr-5 ">Discover More</button>
+              <button className="btn btn-outline btn-secondary mt-2">
+                Latest Project
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+          <a href="#slide4" className="btn btn-circle mr-5">
             ❮
           </a>
-          <a href="#slide2" className="btn btn-circle">
+          <a href="#slide2" className="btn btn-circle ">
             ❯
           </a>
         </div>
       </div>
       <div id="slide2" className="carousel-item relative w-full">
-        <img src={img2} className="w-full" />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide1" className="btn btn-circle">
+        <img src={img2} className="w-full rounded-xl" />
+        <div className="absolute h-full flex items-center  left-0  top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+          <div className="text-white space-y-3 md:space-y-7 pl-12 w-1/2">
+            <h2 className="text-2xl md:text-6xl font-bold">
+              Affordable Price For Car Servicing
+            </h2>
+            <p>
+              There are many variations of passages of available, but the
+              majority have suffered alteration in some form
+            </p>
+            <div>
+              <button className="btn btn-primary mr-5 ">Discover More</button>
+              <button className="btn btn-outline btn-secondary mt-2">
+                Latest Project
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+          <a href="#slide1" className="btn btn-circle mr-5">
             ❮
           </a>
           <a href="#slide3" className="btn btn-circle">
@@ -29,9 +63,26 @@ const Banner = () => {
         </div>
       </div>
       <div id="slide3" className="carousel-item relative w-full">
-        <img src={img3} className="w-full" />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide2" className="btn btn-circle">
+        <img src={img3} className="w-full rounded-xl" />
+        <div className="absolute h-full flex items-center  left-0  top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+          <div className="text-white space-y-3 md:space-y-7 pl-12 w-1/2">
+            <h2 className="text-2xl md:text-6xl font-bold">
+              Affordable Price For Car Servicing
+            </h2>
+            <p>
+              There are many variations of passages of available, but the
+              majority have suffered alteration in some form
+            </p>
+            <div>
+              <button className="btn btn-primary mr-5 ">Discover More</button>
+              <button className="btn btn-outline btn-secondary mt-2">
+                Latest Project
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+          <a href="#slide2" className="btn btn-circle mr-5">
             ❮
           </a>
           <a href="#slide4" className="btn btn-circle">
@@ -40,9 +91,26 @@ const Banner = () => {
         </div>
       </div>
       <div id="slide4" className="carousel-item relative w-full">
-        <img src={img4} className="w-full" />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide3" className="btn btn-circle">
+        <img src={img4} className="w-full rounded-xl" />
+        <div className="absolute h-full flex items-center  left-0  top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+          <div className="text-white space-y-3 md:space-y-7 pl-12 w-1/2">
+            <h2 className="text-2xl md:text-6xl font-bold">
+              Affordable Price For Car Servicing
+            </h2>
+            <p>
+              There are many variations of passages of available, but the
+              majority have suffered alteration in some form
+            </p>
+            <div>
+              <button className="btn btn-primary mr-5 ">Discover More</button>
+              <button className="btn btn-outline btn-secondary mt-2">
+                Latest Project
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+          <a href="#slide3" className="btn btn-circle mr-5">
             ❮
           </a>
           <a href="#slide1" className="btn btn-circle">
